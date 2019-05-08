762dd28ac144bf004e1f4f14f0b93febad4ab8b2
Yong Zhou <jiu-zhou@outlook.com>
2019-05-08 21:38:48 +0800

    重写NgNet.Core

---
+++ NgNet.Core/SystemEnvironment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NgNet
+{
+    public static class SystemEnvironment
+    {
+        public static PlatformTypes PlatformType { get; }
+
+        static SystemEnvironment()
+        {
+
+        }
+    }
+}
